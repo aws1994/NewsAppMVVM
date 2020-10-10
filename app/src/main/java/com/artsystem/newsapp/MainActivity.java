bdf9235a121e793a6c7ceca3e4c2c68bba508cac
aws1994 <awsrhimi@gmail.com>
2020-10-10 03:35:05 -0700

    commit

--- app/src/main/java/com/artsystem/newsapp/MainActivity.java
+++ app/src/main/java/com/artsystem/newsapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.artsystem.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    NavHostFragment navHostFragment;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigation();
+
+    }
+
+    private void setupNavigation() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavigationUI.setupWithNavController(bottomNavigationView,navHostFragment.getNavController());
     }
 }
---
+++ app/src/main/java/com/artsystem/newsapp/NewsFavFragment.java
@@ -0,0 +1,31 @@
+package com.artsystem.newsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NewsFavFragment extends Fragment {
+
+
+    public NewsFavFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_fav, container, false);
+    }
+}
