65bbcf846e06416d558cf655199430d21694ee3e
aws1994 <awsrhimi@gmail.com>
2020-11-10 07:26:31 -0800

    fav/search fragment design

--- app/src/main/java/com/artsystem/newsapp/ui/NewsFavFragment.java
+++ app/src/main/java/com/artsystem/newsapp/ui/NewsFavFragment.java
@@ -2,16 +2,26 @@ package com.artsystem.newsapp.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.artsystem.newsapp.R;
+import com.artsystem.newsapp.data.FakeDataSource;
+import com.artsystem.newsapp.ui.news.DiffUtilNewsItemCallback;
+import com.artsystem.newsapp.ui.news.NewsListAdapter;
 
 
 public class NewsFavFragment extends Fragment {
+
+    private RecyclerView rvFav;
+    private NewsListAdapter adapter;
 
 
     public NewsFavFragment() {
@@ -29,5 +39,23 @@ public class NewsFavFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news_fav, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initFavList(view);
+
+    }
+
+    private void initFavList(@NonNull View view) {
+        rvFav = view.findViewById(R.id.rv_fav);
+        rvFav.setLayoutManager(new GridLayoutManager(getContext(),2));
+        rvFav.setHasFixedSize(true);
+        adapter = new NewsListAdapter(new DiffUtilNewsItemCallback());
+        rvFav.setAdapter(adapter);
+        // get a testing news list from the fake data source
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        adapter.submitList(fakeDataSource.getFakeStaticListNews());
     }
 }
--- app/src/main/java/com/artsystem/newsapp/ui/SearchFragment.java
+++ app/src/main/java/com/artsystem/newsapp/ui/SearchFragment.java
@@ -2,15 +2,25 @@ package com.artsystem.newsapp.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.artsystem.newsapp.R;
+import com.artsystem.newsapp.data.FakeDataSource;
+import com.artsystem.newsapp.ui.news.DiffUtilNewsItemCallback;
+import com.artsystem.newsapp.ui.news.NewsListAdapter;
 
 public class SearchFragment extends Fragment {
+
+    RecyclerView rvSearch;
+    NewsListAdapter adapter;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -27,5 +37,17 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvSearch = view.findViewById(R.id.search_rv);
+        rvSearch.setLayoutManager(new GridLayoutManager(getContext(),2));
+        rvSearch.setHasFixedSize(true);
+        adapter = new NewsListAdapter(new DiffUtilNewsItemCallback());
+        rvSearch.setAdapter(adapter);
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        adapter.submitList(fakeDataSource.getFakeListNews());
     }
 }
